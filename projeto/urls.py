e20321dfbce1f81ab6e623e388e5ee5865cce772
Rafael Amancio <rafael_r_amancio@outlook.com>
2023-12-28 16:07:48 -0300

    Atualizando URL's

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def my_view(requests):
+    return HttpResponse('Respondendo a requisição')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view)
 ]
